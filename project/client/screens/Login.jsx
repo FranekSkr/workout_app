9acb3ab118e24cc75a22205f8f85528e98e23a98
janekskr <janeczek.skrobot@gmail.com>
2023-04-19 19:00:42 +0200

    zmiany

--- project/client/screens/Login.jsx
+++ project/client/screens/Login.jsx
@@ -1,54 +1,78 @@
-import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity, TextInput } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { TextInput } from "react-native-paper";
 
-import {COLORS } from '../assets/dummy'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { COLORS } from "../assets/dummy";
 
 const Login = () => {
+  const [usernameValue, setUsernameValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  handleOnPress = () => {
+    alert(usernameValue);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-       <TextInput
+      <TextInput
         style={styles.input}
-        placeholder="Email"
-        placeholderTextColor={COLORS.white}
+        mode="underlined"
+        label="Username"
+        placeholder="Type a username"
+        value={usernameValue}
+        onChangeText={(usernameValue) => setUsernameValue(usernameValue)}
       />
-       <TextInput
+
+      {/* <TextInput
         style={styles.input}
         placeholder="Password"
         placeholderTextColor={COLORS.white}
-      />
+        onChangeText={(text) => setPasswordValue(text)}
+      /> */}
 
-      <TouchableOpacity >
-        <Text>Submit</Text>
+      <TouchableOpacity style={styles.button} onPress={handleOnPress}>
+        <Text style={styles.buttonText}>Submit</Text>
       </TouchableOpacity>
-       
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: COLORS.lightGrey,
-    flex: 1,  
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
   input: {
     backgroundColor: COLORS.blue,
-    borderRadius: 10,
-    width: '70%',
-    marginVertical: 20,
+    width: "70%",
+    marginVertical: 15,
     paddingVertical: 10,
     paddingHorizontal: 15,
-    fontWeight: 'bold',
-    fontSize: 15,
-    color: COLORS.white
+    fontWeight: "bold",
+    fontSize: 17,
+    color: COLORS.white,
   },
   button: {
     backgroundColor: COLORS.darkBlue,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-  }
-})
+    paddingVertical: 20,
+    width: "40%",
+    borderRadius: 20,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: COLORS.white,
+    textAlign: "center",
+    fontWeight: "800",
+    fontSize: 20,
+  },
+});
 
-export default Login
+export default Login;
