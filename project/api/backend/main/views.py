b40464d7dd21391bccd3c212bac730a8e256ac51
franek <franek.skrobot@gmail.com>
2023-04-28 13:54:54 +0200

    cors authorization fixed

--- project/api/backend/main/views.py
+++ project/api/backend/main/views.py
@@ -10,11 +10,11 @@ from django.contrib import messages
 
 
 # api
-from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+from rest_framework.views import APIView
 
 
 from datetime import date
